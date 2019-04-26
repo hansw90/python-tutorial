587c1e49f7a0b832119e2813e36e43475a1890c6
SEUNGWOO HAN <ashghan@naver.com>
2019-04-26 23:18:15 +0900

    Create pandas01.py
    데이터 분석의 시작 판다스를 알아보자

---
+++ Pandas/pandas01.py
@@ -0,0 +1,15 @@
+"""
+Pandas 란??
+
+Pandas 는 파이썬에서 사용하는 데이터분석 라이브러리로, 행과 열로 이루어진 데이터 객체를 만들어 다룰 수 있게 되며 
+보다 안정적으로 대용량의 데이터들을 처리하는데 매우 편리한 도구이다.
+
+pandas 를 사용하기 위해서는 pandas 를 설치한 이후에 import 를 시켜줘야 한다
+
+import pandas as pd
+
+"""
+
+# pandas 사용하기
+import numpy as np
+import pandas as pd
